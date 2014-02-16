e442d3ff64a190448ec9032dcb73baa4d78ddf1b
justgottrickd <patrick-walsh@uiowa.edu>
2014-02-15 21:26:10 -0600

    For you guys

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject player;
+	//public Vector3 tada = Vector3(3,2,0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void SpawnPlayer(){
+				Instantiate (player, Vector3.zero , Quaternion.identity);
+		}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	public float jumpForce;
+	public float gravity;
+	public float distToGround;
+	public float jumpCount = 1;
+	// Use this for initialization
+	void Start () {
+		distToGround = collider.bounds.extents.y;
+	}
+
+	public bool isGrounded(){
+		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(-speed*Time.deltaTime,0,0);
+		} else if (Input.GetKey (KeyCode.D))
+		{
+			transform.Translate(speed*Time.deltaTime,0,0);
+		}
+		if (Input.GetKeyDown(KeyCode.W) && jumpCount < 2){
+			jumpCount++;
+			rigidbody.AddForce(0,jumpForce,0, ForceMode.Impulse);
+
+		}
+
+		if(isGrounded ())
+			jumpCount =1;
+		rigidbody.AddForce (0, -gravity, 0);
+	}
+}
